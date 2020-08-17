14935d3b29fe8ee08b6ef23e42ccb9429fe3ce74
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-08-17 20:28:54 +0200

    Create calculator in html css

---
+++ script/script.js
@@ -0,0 +1,30 @@
+//functions
+
+// function add(num1, num2){
+//     return num1 + num2;
+// }
+
+const sum = (num1, num2) => num1 + num2;
+const substract = (num1, num2) => num1 - num2;
+const multiply = (num1, num2) => num1 * num2;
+const divide = (num1, num2) => num1 / num2;
+
+const operate = (num1, num2, operator) => {
+    switch (operator) {
+        case '+':
+            return sum(num1, num2);
+            break;
+        case '-':
+            return substract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
