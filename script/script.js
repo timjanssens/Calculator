4401d6308f52cfbef9a916da02c8e24d6ec2bf47
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2020-08-18 19:42:43 +0200

    Adjust max squares

--- script/script.js
+++ script/script.js
@@ -4,8 +4,17 @@
 //     return num1 + num2;
 // }
 
+const textUpEl = document.querySelector('#textUp');
+const textDownEl = document.querySelector('#textDown')
+let outputUp;
+let outputDown; 
+let number1 = '';
+let number2 = '';
+let number1ForCalc = '';
+let firstNum = true;
+
 const sum = (num1, num2) => num1 + num2;
-const substract = (num1, num2) => num1 - num2;
+const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 const divide = (num1, num2) => num1 / num2;
 
@@ -15,7 +24,7 @@ const operate = (num1, num2, operator) => {
             return sum(num1, num2);
             break;
         case '-':
-            return substract(num1, num2);
+            return subtract(num1, num2);
             break;
         case '*':
             return multiply(num1, num2);
@@ -28,4 +37,29 @@ const operate = (num1, num2, operator) => {
     }
 }
 
+const clickBtn = (e) => {
+    if (!isNaN(e.srcElement.id) && firstNum) {
+        number1 += e.srcElement.id;
+        outputDown = number1;
+        textDownEl.textContent = outputDown;
+    }
+    else if (!isNaN(e.srcElement.id) && !firstNum) {
+        number2 += e.srcElement.id;
+        outputDown = number2;
+        textDownEl.textContent = outputDown;
+    }
+    else{
+        textUpEl.textContent = number1;
+        textDownEl.textContent = '';
+        number1ForCalc = number1;
+        number1 = '';
+        
+    }
 
+};
+
+let inputBtn = document.querySelectorAll('.inputBtn');
+
+inputBtn.forEach(() => {
+    addEventListener('click', clickBtn);
+})
